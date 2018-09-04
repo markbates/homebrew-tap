a873e0919a88381679894d728e9d799514e7edd9
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-04 12:21:26 -0400

    Brew formula update for release-test version v1.0.7

--- release-test.rb
+++ release-test.rb
@@ -1,16 +1,16 @@
 class ReleaseTest < Formula
-  desc "me testing goreleaser/homebrew"
+  desc "me testing releases"
   homepage "https://gobuffalo.io"
-  version "0.0.0"
-  url "https://github.com/markbates/release-test/releases/download/v#{version}/release-test_#{version}_darwin_amd64.tar.gz"
-  sha256 "de24228ab2b41cb6423f35193a9616c10562a164b09650d5996a26345c916189"
+  url "https://github.com/markbates/release-test/releases/download/v1.0.7/release-test_1.0.7_Darwin_x86_64.tar.gz"
+  version "1.0.7"
+  sha256 "0e670cedddf87fa0bc5da4c7a828fca118c04f4690ccf09b41f7a58bc02c5dfe"
 
   def install
-    bin.install "buffalo-no-sqlite"
-    mv "#{bin}/buffalo-no-sqlite", "#{bin}/buffalo"
+    bin.install "release-test"
+    mv "#{bin}/release-test", "#{bin}/release-test"
   end
 
   test do
-    system "#{bin}/buffalo", "version"
+    system "#{bin}/release-test", "version"
   end
 end
