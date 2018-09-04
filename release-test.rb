b3154c27291a7794d950b2cb8372f69c6ccdd3be
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-04 15:55:30 -0400

    Brew formula update for release-test version v1.0.13

--- release-test.rb
+++ release-test.rb
@@ -1,9 +1,9 @@
 class ReleaseTest < Formula
   desc "me testing releases"
   homepage "https://gobuffalo.io"
-  url "https://github.com/markbates/release-test/releases/download/v1.0.10/release-test_1.0.10_Darwin_x86_64.tar.gz"
-  version "1.0.10"
-  sha256 "fd5e316929cfffcc2becc4c715fffee5ecc5683a6d1823087df9c6b954ebe5ed"
+  url "https://github.com/markbates/release-test/releases/download/v1.0.13/release-test_1.0.13_Darwin_x86_64.tar.gz"
+  version "1.0.13"
+  sha256 "2a2480ea8c79dbf94fb9c994828929887f0826ae0cb3f4a8eb5f26173c814fa5"
 
   def install
     bin.install "release-test"
