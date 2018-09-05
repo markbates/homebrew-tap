0f0a1005098b9695dbdadc81bedf1ce8baf9698c
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-05 11:53:08 -0400

    Brew formula update for grift version v1.0.2

---
+++ grift.rb
@@ -0,0 +1,11 @@
+class Grift < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/markbates/grift/releases/download/v1.0.2/grift_1.0.2_darwin_amd64.tar.gz"
+  version "1.0.2"
+  sha256 "1075615e2dd2b080b1ed64a75188b1db80e7c8a72e2b9af4fc312cb312238e49"
+
+  def install
+    bin.install "grift"
+  end
+end
