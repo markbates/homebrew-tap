faaf303e2ea104526959bdf9f811b124ed2a01f2
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-17 10:50:26 -0400

    Brew formula update for release-test version v1.1.0-beta.1

--- release-test.rb
+++ release-test.rb
@@ -1,9 +1,9 @@
 class ReleaseTest < Formula
   desc ""
   homepage ""
-  url "https://github.com/markbates/release-test/releases/download/v1.0.31/release-test_1.0.31_darwin_amd64.tar.gz"
-  version "1.0.31"
-  sha256 "e87d59668b9bb0c4f3928456203b1d3df163322f7356ec3c927f307e75d3f9da"
+  url "https://github.com/markbates/release-test/releases/download/v1.1.0-beta.1/release-test_1.1.0-beta.1_darwin_amd64.tar.gz"
+  version "1.1.0-beta.1"
+  sha256 "10f7a537756f59a33f58d30e37f943d503d1adbe187780b6427fbd5e7859f135"
 
   def install
     bin.install "release-test"
