d0f57ced41c8348908318913570f38ab2e71a6d8
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-29 13:06:20 -0400

    Brew formula update for refresh version v1.4.9

--- refresh.rb
+++ refresh.rb
@@ -1,9 +1,9 @@
 class Refresh < Formula
   desc ""
   homepage ""
-  url "https://github.com/markbates/refresh/releases/download/v1.4.8/refresh_1.4.8_darwin_amd64.tar.gz"
-  version "1.4.8"
-  sha256 "6ec079ba9e83565ebe1e3005ff6a9f65ce50c595459b23bba19c0c4c16bab204"
+  url "https://github.com/markbates/refresh/releases/download/v1.4.9/refresh_1.4.9_darwin_amd64.tar.gz"
+  version "1.4.9"
+  sha256 "1febb51cac02a34a6650e4207212f65887293897f079ce94ba08657242464d4f"
 
   def install
     bin.install "refresh"
