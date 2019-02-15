3c3b111ffa9fc8380d973e19dec0afdc41684c80
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-15 10:44:05 -0500

    Brew formula update for deplist version v1.1.3

--- deplist.rb
+++ deplist.rb
@@ -1,9 +1,9 @@
 class Deplist < Formula
   desc ""
   homepage ""
-  url "https://github.com/markbates/deplist/releases/download/v1.1.2/deplist_1.1.2_darwin_amd64.tar.gz"
-  version "1.1.2"
-  sha256 "ce3fe4ce87cd5d5c6174b72a15f0a00e3c5dbaf54ebb690abedb963b444d4271"
+  url "https://github.com/markbates/deplist/releases/download/v1.1.3/deplist_1.1.3_darwin_amd64.tar.gz"
+  version "1.1.3"
+  sha256 "b8a01ce60f3e36e87a9bbbd06651180641323c3f16492ea416b39051351ff476"
 
   def install
     bin.install "deplist"
