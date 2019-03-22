70e091a87766936ee3ca9956e39ead6bb127cbae
goreleaserbot <goreleaser@carlosbecker.com>
2019-03-22 12:36:34 -0400

    Brew formula update for buffalo-heroku version v1.0.6

--- buffalo-heroku.rb
+++ buffalo-heroku.rb
@@ -1,9 +1,9 @@
 class BuffaloHeroku < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/buffalo-heroku/releases/download/v1.0.5/buffalo-heroku_1.0.5_darwin_amd64.tar.gz"
-  version "1.0.5"
-  sha256 "6461d8c66d35eb6277f5fafe880363acc0b2fa82b912f81729ccf80015c30ddc"
+  url "https://github.com/gobuffalo/buffalo-heroku/releases/download/v1.0.6/buffalo-heroku_1.0.6_darwin_amd64.tar.gz"
+  version "1.0.6"
+  sha256 "6343b64b08522a58b13d8c3f57739eab89bc9afe9ec25387ef0e012beaee69f4"
 
   def install
     bin.install "buffalo-heroku"
