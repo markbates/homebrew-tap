1e0a5d9eae1430ae476503fac7c460336524a4cd
goreleaserbot <goreleaser@carlosbecker.com>
2019-06-13 22:28:47 -0500

    Brew formula update for licenser version v1.1.0

--- licenser.rb
+++ licenser.rb
@@ -1,9 +1,9 @@
 class Licenser < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/licenser/releases/download/v1.0.0/licenser_1.0.0_darwin_amd64.tar.gz"
-  version "1.0.0"
-  sha256 "8cf806591fedb1e82fc618ea447befd4c84694d713eda9d8ac863c29f43f8aa2"
+  url "https://github.com/gobuffalo/licenser/releases/download/v1.1.0/licenser_1.1.0_darwin_amd64.tar.gz"
+  version "1.1.0"
+  sha256 "aeeae19875cf10d5bda783a4e8a501efa25c80d008c68c67a04645e8f0558296"
 
   def install
     bin.install "licenser"
