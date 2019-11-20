9370a667d1f36cef387c27889a21bedf4d15cbc0
goreleaserbot <goreleaser@carlosbecker.com>
2019-11-20 06:46:16 -0800

    Brew formula update for clio version v0.1.16

--- clio.rb
+++ clio.rb
@@ -2,25 +2,16 @@
 class Clio < Formula
   desc ""
   homepage ""
-  version "0.1.12"
+  version "0.1.16"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/markbates/clio/releases/download/v0.1.12/clio_0.1.12_darwin_amd64.tar.gz"
-    sha256 "bb36e127ef0e754fbd10bbaee783aa24a5a8dcb2bd0b388be87f034e80b3c5e9"
+    url "https://github.com/markbates/clio/releases/download/v0.1.16/clio_0.1.16_Darwin_x86_64.tar.gz"
+    sha256 "fb439d7761ee24c63a45baddfde1f11eab0b7276610006c65570135d27023cd3"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/markbates/clio/releases/download/v0.1.12/clio_0.1.12_linux_amd64.tar.gz"
-      sha256 "719d2ac54657a8dfd88acf0fb949a9c814446a00d47fd1afdde21fc660092fc6"
-    end
-    if Hardware::CPU.arm?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/markbates/clio/releases/download/v0.1.12/clio_0.1.12_linux_arm64.tar.gz"
-        sha256 "04e37267b6d8b3c86b7b1326b8b305c8313917f41a41390b289bc5cbc03a8b59"
-      else
-        url "https://github.com/markbates/clio/releases/download/v0.1.12/clio_0.1.12_linux_armv6.tar.gz"
-        sha256 "30437d116d879fa2f6aba6061638e0c622cb1427aa6611f0199c9ceedc822dd2"
-      end
+      url "https://github.com/markbates/clio/releases/download/v0.1.16/clio_0.1.16_Linux_x86_64.tar.gz"
+      sha256 "110fa958f7e288b4247c78015af6000bf0e87f4fd0262949d44eb9b61727579d"
     end
   end
 
