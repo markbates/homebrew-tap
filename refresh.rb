984043475444ddfa79b5a74db8aa911e6528f98b
goreleaserbot <goreleaser@carlosbecker.com>
2020-03-08 13:36:31 -0400

    Brew formula update for refresh version v1.11.1

--- refresh.rb
+++ refresh.rb
@@ -2,24 +2,24 @@
 class Refresh < Formula
   desc ""
   homepage ""
-  version "1.11.0"
+  version "1.11.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/markbates/refresh/releases/download/v1.11.0/refresh_1.11.0_Darwin_x86_64.tar.gz"
-    sha256 "d3fc14e7635fd4e3615898f9d5c67178ca4c2b500d182c4068ea6bed3f36c5d9"
+    url "https://github.com/markbates/refresh/releases/download/v1.11.1/refresh_1.11.1_Darwin_x86_64.tar.gz"
+    sha256 "ef3fdf59a1e7bfe995381990b840f8fdcef5404affdd1b69348ae5f488eb4c0d"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/markbates/refresh/releases/download/v1.11.0/refresh_1.11.0_Linux_x86_64.tar.gz"
-      sha256 "9c66d20587b7b89141ee177c9691005c6588608b8241f6e496f7ad7de2a6cd6c"
+      url "https://github.com/markbates/refresh/releases/download/v1.11.1/refresh_1.11.1_Linux_x86_64.tar.gz"
+      sha256 "fe7b5ab198b4bcb73d3f4f594c5737488ad3ee5752f5f5c514f2e1087110bd09"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/markbates/refresh/releases/download/v1.11.0/refresh_1.11.0_Linux_arm64.tar.gz"
-        sha256 "e5f7750bd5f36384e9acd794e17147f90dd3f2837444d896ca2c2069dce45376"
+        url "https://github.com/markbates/refresh/releases/download/v1.11.1/refresh_1.11.1_Linux_arm64.tar.gz"
+        sha256 "5584a8373b54094ee9f48b7cb36b7173afcf23300c8f6172b182a2a9404ea74c"
       else
-        url "https://github.com/markbates/refresh/releases/download/v1.11.0/refresh_1.11.0_Linux_armv6.tar.gz"
-        sha256 "b623eca94589826cb2d6c9af9d4e916ec4ab7a5f054127a87196f0a3339ca7e0"
+        url "https://github.com/markbates/refresh/releases/download/v1.11.1/refresh_1.11.1_Linux_armv6.tar.gz"
+        sha256 "6bf4bcbda4c36df71045cd7b15005a62e2dbd02957b9ee2c2e19ab54853760df"
       end
     end
   end
